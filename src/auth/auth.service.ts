256c190676243a837d9289573330e4a67511c474
Diaa Abusaada <dia.saada@gmail.com>
2022-11-28 13:59:34 +0300

    auth skelton

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@nestjs/common";
+
+
+@Injectable()
+export class AuthService
+{
+    signin() {
+        throw new Error("signin Method not implemented.");
+    }
+    signup() {
+        throw new Error("signup Method not implemented.");
+    }
+
+}
